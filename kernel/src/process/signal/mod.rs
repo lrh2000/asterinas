6963aafc80b0e2758c0c77a9bc33fb2c20047807
Ruihan Li <lrh2000@pku.edu.cn>
2026-07-06 01:11:06 +0800

    Support vDSO in AArch64

--- kernel/src/process/signal/mod.rs
+++ kernel/src/process/signal/mod.rs
@@ -426,7 +426,7 @@ pub fn handle_user_signal(
         restorer_addr
     } else {
         cfg_if::cfg_if! {
-            if #[cfg(target_arch = "riscv64")] {
+            if #[cfg(any(target_arch = "riscv64", target_arch = "aarch64"))] {
                 ctx.user_space().vmar().process_vm().vdso_base()
                     + crate::vdso::__VDSO_RT_SIGRETURN_OFFSET
             } else {
